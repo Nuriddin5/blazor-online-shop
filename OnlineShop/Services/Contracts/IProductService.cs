a3b1c06e65251a18c067ce6e311e276ed4d136cc
Nuriddin5 <nuriddinqurbonboyev@mail.ru>
2022-11-13 02:07:22 +0500

    added service to blazor app

---
+++ OnlineShop/Services/Contracts/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using OnlineShop.Models.Dtos;
+
+namespace OnlineShop.Services.Contracts;
+
+public interface IProductService
+{
+    Task<IEnumerable<ProductDto>> GetItems();
+    
+}
---
+++ OnlineShop/Services/Contracts/ProductServices.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Net.Http.Json;
+using OnlineShop.Models.Dtos;
+
+namespace OnlineShop.Services.Contracts;
+
+public class ProductServices : IProductService
+{
+    private readonly HttpClient _httpClient;
+
+    public ProductServices(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<IEnumerable<ProductDto>> GetItems()
+    {
+        try
+        {
+            var products = await _httpClient.GetFromJsonAsync<IEnumerable<ProductDto>>("api/Product");
+            return products;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
