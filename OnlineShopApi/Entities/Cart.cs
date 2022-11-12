bf455dce25f306211afe28c99c205b839b65a1f1
Nuriddin5 <nuriddinqurbonboyev@mail.ru>
2022-11-13 01:25:41 +0500

    Bismillah

---
+++ OnlineShopApi/Entities/Cart.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineShopApi.Entities
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        
+        
+    }
+}
---
+++ OnlineShopApi/Entities/ProductCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineShopApi.Entities
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string IconCSS { get; set; }
+    }
+}
